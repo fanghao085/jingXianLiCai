0d673a09edc4ddaee47ea95db06b8f83ad14cb5b
fanghao085 <fanghao085@Gmail.com>
2017-03-14 15:50:13 +0800

    12345678

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ixjt on 2017/3/14.
+//  Copyright © 2017年 ixjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by ixjt on 2017/3/14.
+//  Copyright © 2017年 ixjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
