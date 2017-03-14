d1fab9c65c6adeec3d1acdb6d2f7820267b7bc48
fanghao085 <fanghao085@Gmail.com>
2017-03-14 16:09:54 +0800

    aaaa a

---
+++ test/test/aaaa.h
@@ -0,0 +1,13 @@
+//
+//  aaaa.h
+//  test
+//
+//  Created by 京县理财iMac3 on 17/3/14.
+//  Copyright © 2017年 ixjt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface aaaa : NSObject
+
+@end
---
+++ test/test/aaaa.m
@@ -0,0 +1,13 @@
+//
+//  aaaa.m
+//  test
+//
+//  Created by 京县理财iMac3 on 17/3/14.
+//  Copyright © 2017年 ixjt. All rights reserved.
+//
+
+#import "aaaa.h"
+
+@implementation aaaa
+
+@end
